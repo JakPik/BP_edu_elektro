ff3164574f8384ddbfac30b6c77a8982f45b05e5
Jakub Pikal <jakub.pikal@gmail.com>
2026-04-28 13:59:56 +0200

    Fixed grab logic + asociated bugs

--- EduElectroCircuit/Assets/Scripts/Player/PlayerInteractionControl.cs
+++ EduElectroCircuit/Assets/Scripts/Player/PlayerInteractionControl.cs
@@ -6,26 +6,38 @@ using UnityEngine.InputSystem;
 public struct GrabbedObject
 {
     public GameObject component;
+    public Rigidbody object_rb;
     public IGrabable grabable;
 
     public void Refresh()
     {
         component = null;
         grabable = null;
+        object_rb = null;
     }
 }
 
 public class PlayerInteractionControl : MonoBehaviour
 {
     [SerializeField] private CameraControl cameraControl;
+    [Header("Hold/Grab params")]
+    [SerializeField] private float followSpeed = 10f;
+    [SerializeField] private float smoothness = 15f;
+    [SerializeField] private float dropDistance = 1f;
     [SerializeField] private float holdDistance = 2.0f;
+    [Space(10)]
     [SerializeField] private bool holding = false;
     [SerializeField] private GrabbedObject grabbedObject = new GrabbedObject();
+    [Space(10)]
     [SerializeField] private IInteractable interactable;
+
+    [Space(10)]
+    [Header("Player actions")]
     [SerializeField] private InputActionReference interactAction;
     [SerializeField] private InputActionReference holdtAction;
     [Header("Event Channels")]
     [SerializeField] private GenericEventChannel<ReloadEvent> reload;
+
 
     private void Update()
     {
@@ -33,14 +45,26 @@ public class PlayerInteractionControl : MonoBehaviour
         UpdateHold();
     }
 
+
     private void UpdateHold()
     {
         if (holding && grabbedObject.component != null)
         {
-            Ray ray = new Ray(cameraControl.GetCameraPosition(), cameraControl.GetCameraForward());
-            LayerMask mask = ~LayerMask.GetMask("Grabable");
-            float distance = Physics.Raycast(ray, out RaycastHit hitInfo, holdDistance, mask, QueryTriggerInteraction.Ignore) ? hitInfo.distance : holdDistance;
-            grabbedObject.component.transform.position = cameraControl.GetCameraForward() * distance + cameraControl.GetCameraPosition();
+            Vector3 newPos = cameraControl.GetCameraForward() * holdDistance + cameraControl.GetCameraPosition();
+            Vector3 direction = newPos - grabbedObject.component.transform.position;
+            Vector3 targetVelocity = direction * followSpeed;
+
+            if (Math.Abs(direction.magnitude) > dropDistance)
+            {
+                Drop();
+                return;
+            }
+
+            grabbedObject.object_rb.linearVelocity = Vector3.Lerp(
+                grabbedObject.object_rb.linearVelocity,
+                targetVelocity,
+                smoothness * Time.deltaTime
+            );
         }
     }
 
@@ -57,24 +81,32 @@ public class PlayerInteractionControl : MonoBehaviour
 
             interactable?.DisplayInfo(false);
             interactable = null;
+            return;
         }
-        else
+
+        GameObject obj = hitInfo.collider.gameObject;
+        if (obj.TryGetComponent(out IInteractable interact) && interact != interactable)
         {
-            GameObject obj = hitInfo.collider.gameObject;
-            if (obj.TryGetComponent(out IInteractable interact) && interact != interactable)
+            interactable?.DisplayInfo(false);
+            interactable = interact;
+            interactable.DisplayInfo(true);
+        }
+        if (!holding && obj != grabbedObject.component && obj.TryGetComponent(out IGrabable grabable))
+        {
+            grabbedObject.grabable?.DisplayInfo(false);
+            grabbedObject.component = hitInfo.collider.gameObject;
+            grabbedObject.grabable = grabable;
+            grabbedObject.object_rb = hitInfo.collider.gameObject.GetComponent<Rigidbody>();
+            if (grabbedObject.object_rb == null)
             {
-                interactable?.DisplayInfo(false);
-                interactable = interact;
-                interactable.DisplayInfo(true);
+                grabbedObject.Refresh();
             }
-            if (obj != grabbedObject.component && obj.TryGetComponent(out IGrabable grabable))
+            else
             {
-                grabbedObject.grabable?.DisplayInfo(false);
-                grabbedObject.component = hitInfo.collider.gameObject;
-                grabbedObject.grabable = grabable;
                 grabable.DisplayInfo(true);
             }
         }
+
     }
 
     private void OnEnable()
@@ -106,22 +138,37 @@ public class PlayerInteractionControl : MonoBehaviour
 
     private void OnHold(InputAction.CallbackContext context)
     {
-        if (!grabbedObject.grabable?.CanGrab() ?? true) return;
+        if ((!holding && grabbedObject.component == null) || (!grabbedObject.grabable?.CanGrab() ?? true)) return;
+        if (!holding)
+        {
+            Grab();
+        }
+        else
+        {
+            Drop();
+        }
+    }
+
+    private void Grab()
+    {
+        holding = !holding;
         try
         {
-            grabbedObject.grabable.OnGrab(!holding, this.gameObject);
+            grabbedObject.grabable.OnGrab(true, this.gameObject);
             grabbedObject.grabable?.DisplayInfo(false);
         }
         catch (Exception e)
         {
+            holding = !holding;
             Logger.Log(this, this.name, "While calling OnGrab() on " + grabbedObject.component.name + "\n" + e.Message, LogType.EXCEPTION);
         }
-        if (holding)
-        {
-            grabbedObject.Refresh();
-        }
-        if (!holding && grabbedObject.component == null) return;
+    }
+
+    private void Drop()
+    {
         holding = !holding;
+        grabbedObject.grabable.OnGrab(false, this.gameObject);
+        grabbedObject.Refresh();
     }
 
     private void OnInteract(InputAction.CallbackContext context)
